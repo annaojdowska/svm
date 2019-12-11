287cb2f5ba23fd6da0d935f7eea5d78421c2c088
Anna Ojdowska <annaojdowska@gmail.com>
2019-12-11 22:47:01 +0100

    Optimization by hyperplane

--- main.py
+++ main.py
@@ -3,35 +3,36 @@ import matplotlib.cm as cm
 import numpy as np
 import itertools
 from optimisation.trainer import Trainer
-from optimisation.kernel import Kernel
-from iterative.classificator import SVM
+from svm.kernel import Kernel
+#from iterative.classificator import SVM
+from svm.binary_classes import SVM
 from sklearn.datasets.samples_generator import make_blobs
 from sklearn.model_selection import train_test_split
-from svm.binary_classes import binary_classification_smo
+#from svm.binary_classes import binary_classification_smo
 
 
 def main():
     # example()
-    generateDataCVXOPT()
+    example()
 
 
-def generateDataManual():
-    X, y = make_blobs(n_samples=250, centers=2, random_state=0, cluster_std=0.60)
-    y[y == 0] = -1
-    tmp = np.ones(len(X))
-    y = tmp * y
-    plt.scatter(X[:, 0], X[:, 1], c=y, cmap='winter')
-    plt.show()
-
-    X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=0)
-    classifier = binary_classification_smo(Kernel.linear())
-    classifier.fit(X_train, y_train)
-    sup_vec = classifier.support_vectors_
-    plt.plot(sup_vec)
-    plt.show()
+# def generateDataManual():
+#     X, y = make_blobs(n_samples=250, centers=2, random_state=0, cluster_std=0.60)
+#     y[y == 0] = -1
+#     tmp = np.ones(len(X))
+#     y = tmp * y
+#     plt.scatter(X[:, 0], X[:, 1], c=y, cmap='winter')
+#     plt.show()
+#
+#     X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=0)
+#     classifier = binary_classification_smo(Kernel.linear())
+#     classifier.fit(X_train, y_train)
+#     sup_vec = classifier.support_vectors_
+#     plt.plot(sup_vec)
+#     plt.show()
 
 def generateDataCVXOPT():
-    X, y = make_blobs(n_samples=250, centers=2, random_state=0, cluster_std=0.70)
+    X, y = make_blobs(n_samples=250, centers=2, random_state=0, cluster_std=0.60)
     y[y == 0] = -1
     tmp = np.ones(len(X))
     y = tmp * y
@@ -68,12 +69,23 @@ def generateDataCVXOPT():
     plt.show()
 
 
-def example(num_samples=10, num_features=2, grid_size=20):
-    samples = np.matrix(np.random.normal(size=num_samples * num_features).reshape(num_samples, num_features))
+def example(num_samples=100, num_features=2, grid_size=200):
+    samples = np.array(np.random.normal(size=num_samples * num_features).reshape(num_samples, num_features))
     labels = 2 * (samples.sum(axis=1) > 0) - 1.0
-    trainer = Trainer(Kernel.linear(), 0.0001)
-    predictor = trainer.train(samples, labels)
-    plot(predictor, samples, labels, grid_size)
+
+    # np.random.seed(0)
+    # samples = np.r_[np.random.randn(50, 2) - [2, 2], np.random.randn(50, 2) + [2, 2]]
+    # labels = [0] * 50 + [1] * 50
+
+    # samples, labels = make_blobs(n_samples=num_samples, centers=num_features, random_state=0, cluster_std=0.60)
+    # labels[labels == 0] = -1
+    # tmp = np.ones(len(samples))
+    # labels = tmp * labels
+
+    clf = SVM(Kernel.linear(), 0.1)
+    clf.fit(samples, labels)
+    print(clf.score(samples, labels))
+    plot(clf, samples, labels, grid_size)
 
 
 def plot(predictor, X, y, grid_size):
@@ -82,7 +94,7 @@ def plot(predictor, X, y, grid_size):
     xx, yy = np.meshgrid(np.linspace(x_min, x_max, grid_size),
                          np.linspace(y_min, y_max, grid_size),
                          indexing='ij')
-    flatten = lambda m: np.array(m).reshape(-1, )
+    flatten = lambda m: np.array(m).reshape(-1,)
 
     result = []
     for (i, j) in itertools.product(range(grid_size), range(grid_size)):
@@ -90,17 +102,54 @@ def plot(predictor, X, y, grid_size):
         result.append(predictor.predict(point))
 
     Z = np.array(result).reshape(xx.shape)
+    plt.clf()
 
-    # plt.contourf(xx, yy, Z,
-    #              cmap=cm.Paired,
-    #              levels=[-0.001, 0.001],
-    #              extend='both',
-    #              alpha=0.8)
+    plt.contourf(xx, yy, Z,
+                 cmap=cm.Paired,
+                 levels=[-0.001, 0.001],
+                 extend='both',
+                 alpha=0.8)
     plt.scatter(flatten(X[:, 0]), flatten(X[:, 1]),
                 c=flatten(y), cmap=cm.Paired)
+
+    def f(x, w, b, c=0):
+        return (-w[0] * x - b + c) / w[1]
+
+    # # w.x + b = 0
+    # a0 = -4
+    # a1 = f(a0, predictor.w, predictor.intercept_)
+    # b0 = 4
+    # b1 = f(b0, predictor.w, predictor.intercept_)
+    # plt.plot([a0, b0], [a1, b1], 'k')
+    # # w.x + b = 1
+    # a0 = -10
+    # a1 = f(a0, predictor.w, predictor.intercept_, 1)
+    # b0 = 10
+    # b1 = f(b0, predictor.w, predictor.intercept_, 1)
+    # plt.plot([a0, b0], [a1, b1], 'k--')
+    # # w.x + b = -1
+    # a0 = -10
+    # a1 = f(a0, predictor.w, predictor.intercept_, -1)
+    # b0 = 10
+    # b1 = f(b0, predictor.w, predictor.intercept_, -1)
+    # plt.plot([a0, b0], [a1, b1], 'k--')
+    w = predictor.w
+    a = -w[0] / w[1]
+    xx = np.linspace(-10, 10)
+    yy = a * xx - predictor.intercept_ / w[1]
+    plt.plot(xx, yy, 'k-')
+    b = predictor.support_vectors_[0]
+    yy_down = a * xx + (b[1] - a * b[0])
+    b = predictor.support_vectors_[-1]
+    yy_up = a * xx + (b[1] - a * b[0])
+
+    # plot the line, the points, and the nearest vectors to the plane
+    plt.plot(xx, yy, 'k-')
+    plt.plot(xx, yy_down, 'k--')
+    plt.plot(xx, yy_up, 'k--')
+
     plt.xlim(x_min, x_max)
     plt.ylim(y_min, y_max)
     plt.show()
-
 
 main()
--- svm/__init__.py
+++ svm/__init__.py
@@ -1,13 +1,13 @@
-from .binary_classes import (
-    binary_classification_qp,
-    binary_classification_smo,
-)
+# from .binary_classes import (
+#     # binary_classification_qp,
+#     # binary_classification_smo,
+# )
 
-from .multi_classes import (
-    #multiclass_ova,
-    multiclass_ovo,
-)
-
-from .kernel import (
-    Kernel,
-)
+# from .multi_classes import (
+#     #multiclass_ova,
+#     multiclass_ovo,
+# )
+#
+# from .kernel import (
+#     Kernel,
+# )
